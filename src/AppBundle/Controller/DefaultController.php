d47cdd057f01c23af600cd0012af81cd43773758
Renato Cadecaro <rcadecaro@gmail.com>
2015-06-23 19:31:03 -0300

    Projeto Parte um - Rotas e parametros get

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/{nome}", name="homepage")
+     */
+    public function indexAction($nome)
+    {
+        return $this->render('AppBundle:Default:index.html.twig', ['nome'=>$nome]);
+    }
+    /**
+     * @Route("/run/{marca}/{modelo}", name="testdrive")
+     */
+    public function testDriveAction($marca, $modelo)
+    {
+        return $this->render('AppBundle:Default:testdrive.html.twig', ['marca'=>$marca, 'modelo'=>$modelo]);
+    }
+}
